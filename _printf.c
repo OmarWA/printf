00cf2dd5ab76fe1df1e49d51ec318b79ece2d417
Omar Wael <omarwael19@gmail.com>
2023-11-14 10:49:25 -0800

    Printing a simple sentence using our custom printf

--- _printf.c
+++ _printf.c
@@ -2,8 +2,18 @@
 
 int _printf(const char *format, ...)
 {
+  int printedChars = 0;
+  va_list argumentPointer;
+  
+  va_start(argumentPointer, format);
 
   
+  
+  
+ printedChars =  write(1, format, strlen(format));
+
+
+ return printedChars; 
 
   
 }
--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 
