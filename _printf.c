3e2aade5d9fa1512735c4db51497c5bb41ebc6dd
Omar Wael <omarwael19@gmail.com>
2023-11-14 11:40:42 -0800

    proping the string for its end NUL character

--- _printf.c
+++ _printf.c
@@ -4,13 +4,18 @@ int _printf(const char *format, ...)
 {
   int printedChars = 0;
   va_list argumentPointer;
+  int stringIndex = 0;
+  /* int argumentsCount = 0;*/
   
   va_start(argumentPointer, format);
 
+  while (format[stringIndex] != '\0')
+   {
+      printf("char: %c$; ASCII: %d\n", format[stringIndex], format[stringIndex]);
+  stringIndex++;
+  }
   
-  
-  
- printedChars =  write(1, format, strlen(format));
+  /* printedChars =  write(1, format, strlen(format));*/
 
 
  return printedChars; 
--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-  _printf("Hello, Printf!\n");
+  _printf("This is a long simple statement that tests how my custom printf function would recognize the end of a string\n");
   return (0);
 }
